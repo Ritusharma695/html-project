3293474f4711eeeaba24cf7526c2c0438b397bdc
Ritusharma695 <as8410677@gmail.com>
2026-06-25 20:36:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ async function getWeather(){
 
     const city=document.getElementById("city").value;
 
-    const apiKey="YOUR_API_KEY";
+    const apiKey="2657e145d2990a24ba6ee7e2bb93bcb2";
 
     const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
